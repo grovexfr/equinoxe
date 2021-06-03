0cf3203482fc3f3c28e6b620ba16ed6bcfdfeed8
grovexfr <85291598+grovexfr@users.noreply.github.com>
2021-06-03 23:32:20 +0200

    initiale

---
+++ equinoxe.lua
@@ -0,0 +1,16 @@
+local u = HodorReflexes.users
+local a = HodorReflexes.anim.users
+
+u["@dov118"] = {"dov118", "|c0060ffdo|r|cffffffv1|r|cff000018|r", "HodorReflexes/users/equinoxe/dov118.dds"}
+u["@Wyrelll"] = {"Wyrelll", "|c0060ffWy|r|cffffffre|r|cff0000lll|r", "HodorReflexes/users/equinoxe/Wyrelll.dds"}
+u["@Olemike"] = {"Olemike", "|cb80113Olemike|r", "HodorReflexes/users/equinoxe/Olemike.dds"}
+u["@Matbax"] = {"Matbax", "|cff00f6L|r|ca800ffe|r |c0030ffM|r|c00ffc0a|r|c00ff06t|r|cfff000b|r|cff8400a|r|cff000ax|r", "HodorReflexes/users/equinoxe/Matbax.dds"}
+u["@minou29"] = {"Minou", "|c3b7e86Minou|r", "HodorReflexes/users/equinoxe/minou29.dds"}
+u["@Kalior"] = {"Kalior", "|c0060ffKa|r|cffffffli|r|cff0000or|r", "HodorReflexes/users/equinoxe/Kalior.dds"}
+u["@grovex_fr"] = {"MissLijah", "|cFFFFFFMiss|r|cC21075L|r|cCE2580i|r|cDA3B8Bj|r|cE65196a|r|cF267A1h|r", "HodorReflexes/users/equinoxe/Misslijah.dds"}
+u["@StickyKiwi"] = {"StickyKiwi", "|c0C007DS|r|c24198At|r|c3C3397i|r|c544CA4c|r|c6D66B1k|r|c857FBEy|r|c9D99CBK|r|cB6B2D8i|r|cCECCE5w|r|cE6E5F2i|r", "HodorReflexes/users/equinoxe/StickyKiwi.dds"}
+u["@sdqsd"] = {"grovex_fr", "|c230AFFg|r|c5A47FFr|r|c9184FFo|r|cC8C1FFv|r|cFFFFFFe|r|cFFC1C1x|r|cFF8484_|r|cFF4747f|r|cFF0A0Ar|r", "HodorReflexes/users/equinoxe/grovex.dds"}
+u["@helyos16"] = {"helyos16", "|c0BB31BHelyos16|r", "HodorReflexes/users/equinoxe/helyos16.dds"}
+u["@P'titVal"] = {"P'tit Val", "|cFFFFFFP|r|cFFBFBF'|r|cFF7F7Ft|r|cFF3F3Fi|r|cFF0000t|r|cFF3F3F |r|cFF7F7FV|r|cFFBFBFa|r|cFFFFFFl|r", "HodorReflexes/users/equinoxe/PtitVal.dds"}
+u["@TerraOct"] = {"TerraOct", "|c6408CCTerraOct|r", "HodorReflexes/users/equinoxe/TerraOct.dds"}
+u["@LadyChiv"] = {"LadyChiv", "|cFFFFFFL|r|cFFF9C4a|r|cFFF389d|r|cFFED4Ey|r|cFFE814C|r|cFFED4Eh|r|cFFF389i|r|cFFF9C4v|r", "HodorReflexes/users/equinoxe/LadyChiv.dds"}

85bb027f5d418d63f176e46b30f90a5a4d7c3134
grovexfr <85291598+grovexfr@users.noreply.github.com>
2021-06-04 22:00:58 +0200

    Add files via upload

---
+++ Elder Scrolls Online/live/AddOns/HodorReflexes/users/equinoxe/equinoxe.lua
@@ -0,0 +1,22 @@
+local u = HodorReflexes.users
+local a = HodorReflexes.anim.users
+
+u["@dov118"] = {"dov118", "|c0060ffdo|r|cffffffv1|r|cff000018|r", "HodorReflexes/users/equinoxe/dov118.dds"}
+u["@Wyrelll"] = {"Wyrelll", "|c0060ffWy|r|cffffffre|r|cff0000lll|r", "HodorReflexes/users/equinoxe/Wyrelll.dds"}
+u["@Olemike"] = {"Olemike", "|cb80113Olemike|r", "HodorReflexes/users/equinoxe/Olemike.dds"}
+u["@Matbax"] = {"Matbax", "|cff00f6L|r|ca800ffe|r |c0030ffM|r|c00ffc0a|r|c00ff06t|r|cfff000b|r|cff8400a|r|cff000ax|r", "HodorReflexes/users/equinoxe/Matbax.dds"}
+u["@minou29"] = {"Minou", "|c3b7e86Minou|r", "HodorReflexes/users/equinoxe/minou29.dds"}
+u["@Kalior"] = {"Kalior", "|c0060ffKa|r|cffffffli|r|cff0000or|r", "HodorReflexes/users/equinoxe/Kalior.dds"}
+u["@MissLijah"] = {"MissLijah", "|cFFFFFFMiss|r|cC21075L|r|cCE2580i|r|cDA3B8Bj|r|cE65196a|r|cF267A1h|r", "HodorReflexes/users/equinoxe/Misslijah.dds"}
+u["@StickyKiwi"] = {"StickyKiwi", "|c0C007DS|r|c24198At|r|c3C3397i|r|c544CA4c|r|c6D66B1k|r|c857FBEy|r|c9D99CBK|r|cB6B2D8i|r|cCECCE5w|r|cE6E5F2i|r", "HodorReflexes/users/equinoxe/StickyKiwi.dds"}
+u["@grovex_fr1"] = {"grovex_fr", "|cFF0A0Ag|r|cFF4747r|r|cFF8484o|r|cFFC1C1v|r|cFFFFFFe|r|cFFC1C1x|r|cFF8484_|r|cFF4747f|r|cFF0A0Ar|r", "HodorReflexes/users/equinoxe/grovex.dds"}
+u["@Helyos16"] = {"Helyos16", "|c0BB31BHelyos16|r", "HodorReflexes/users/equinoxe/Helyos16.dds"}
+u["@P'titVal"] = {"P'tit Val", "|cFFFFFFP|r|cFFBFBF'|r|cFF7F7Ft|r|cFF3F3Fi|r|cFF0000t|r|cFF3F3F |r|cFF7F7FV|r|cFFBFBFa|r|cFFFFFFl|r", "HodorReflexes/users/equinoxe/PtitVal.dds"}
+u["@TerraOct"] = {"TerraOct", "|c6408CCTerraOct|r", "HodorReflexes/users/equinoxe/TerraOct.dds"}
+u["@LadyChiv"] = {"LadyChiv", "|cFFFFFFL|r|cFFF9C4a|r|cFFF389d|r|cFFED4Ey|r|cFFE814C|r|cFFED4Eh|r|cFFF389i|r|cFFF9C4v|r", "HodorReflexes/users/equinoxe/LadyChiv.dds"}
+u["@zebio22"] = {"zebio22", "|cC1C2B2zebio22|r", "HodorReflexes/users/equinoxe/zebio22.dds"}
+u["@grovex_fr"] = {"StickeyBench", "|c000000S|r|c2A0000t|r|c550000i|r|c7F0000c|r|cAA0000k|r|cD40000e|r|cFF0000y|r|cFD2A00B|r|cFC5500e|r|cFA7F00n|r|cF9AA00c|r|cF8D400h|r", "HodorReflexes/users/equinoxe/Bench.dds"}
+u["@luxferre22"] = {"luxferre22", "|cFF0000LUX|r", "HodorReflexes/users/equinoxe/luxferre22.dds"}
+u["@Hettyk.Haran"] = {"Hettyk Haran", "|c0000B3H|r|c2A0095e|r|c550077t|r|c7F0059t|r|cAA003By|r|cD4001DK|r|cFF0007 |r|cFD2A00H|r|cFC5500a|r|cFA7F00r|r|cF9AA00a|r|cF8D400n|r", "HodorReflexes/users/equinoxe/Hettyk.dds"}
+u["@sanstitre01"] = {"sanstitre01", "|cE600FFN@ath <3|r", "HodorReflexes/users/equinoxe/sanstitre01.dds"}
+u["@Divine69"] = {"Divine", "|cFDEBA7Divine|r", "HodorReflexes/users/equinoxe/Divine69.dds"}
